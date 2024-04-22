a82d0a36f78759f5bdb8ddc47a8efa87ae246dc6
shilangyu <xmarcinmarcin@gmail.com>
2024-04-22 18:36:36 +0200

    Move theorems about functions

---
+++ lisa-sets/src/main/scala/lisa/maths/settheory/functions/Functions.scala
@@ -0,0 +1,11 @@
+package lisa.maths.settheory.functions
+
+/**
+ * Set-theoretic functions library
+ *
+ * Develops the set-theoretic functions, their properties, and common theorems.
+ */
+object Functions {
+  export lisa.maths.settheory.functions.Functionals.*
+  export lisa.maths.settheory.functions.FunctionPoperties.*
+}
